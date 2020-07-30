84962183cea7c11fc0ee12c3c36108efd69354ad
wangwenyin <wangweny.kok@foxmail.com>
2020-07-30 16:50:11 +0800

    add

---
+++ fxxt静态页面/mobile/js/rem.js
@@ -0,0 +1,20 @@
+// rem适配：设置根节点字体大小
+(function () {
+    var timer = null;
+    var deviceScale = 7.5;
+    setRootFontSize();
+
+    function setRootFontSize() {
+        var width = document.documentElement.clientWidth;
+        var styleN = document.createElement('style');
+        styleN.innerHTML = 'html{font-size:' + width / deviceScale + 'px}';
+        document.head.appendChild(styleN);
+    };
+
+    window.addEventListener('resize', function () {
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+            setRootFontSize()
+        }, 300)
+    })
+})();
